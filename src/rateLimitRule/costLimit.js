bc102dc3ae4db89320b5e887aea8e18bd63a43af
roldma <roldma@farmingdale.edu>
2018-08-22 20:22:02 -0400

    prettified

--- src/rateLimitRule/costLimit.js
+++ src/rateLimitRule/costLimit.js
@@ -1,104 +1,91 @@
 //@flow
+
 import type {
-	ValidationContext,
-	FragmentDefinitionNode,
-	OperationDefinitionNode,
-	FieldNode,
-	FragmentSpreadNode,
-	InlineFragmentNode
+  ValidationContext,
+  FragmentDefinitionNode,
+  OperationDefinitionNode,
+  FieldNode,
+  FragmentSpreadNode,
+  InlineFragmentNode,
 } from 'graphql';
 
 export type costComplexityOptions = {
-	costLimit: number,
-	onSuccess ?: (depth:number)=>void,
-	onError ?:(actual:number,maxDepth:number)=>GraphQLError
-}
+  costLimit: number,
+  onSuccess?: (depth: number) => void,
+  onError?: (actual: number, maxDepth: number) => GraphQLError,
+};
 
 const graphql = require('graphql');
-const {
-	GraphQLError,
-} = graphql;
+const { GraphQLError } = graphql;
 
+class CostLimitComplexity {
+  context: ValidationContext;
+  rateLimit: number;
+  OperationDefinition: Object;
+  argsArray: Array<number>;
+  cost: number;
 
-class CostLimitComplexity{
-	context:ValidationContext;
-	rateLimit:number;
-	OperationDefinition: Object;
-	argsArray: Array<number>;
-	cost:number;
+  constructor(context: ValidationContext, config: RateLimitOptions) {
+    this.context = context;
 
-	constructor(context:ValidationContext, config:RateLimitOptions){
-		this.context = context;
+    this.argsArray = [];
+    this.cost = 0;
+    this.config = config;
 
-		this.argsArray = [];
-		this.cost = 0;
-		this.config = config;
+    this.OperationDefinition = {
+      enter: this.onOperationDefinitionEnter,
+      leave: this.onOperationDefinitionLeave,
+    };
+  }
 
-		this.OperationDefinition = {
-			enter:this.onOperationDefinitionEnter,
-			leave:this.onOperationDefinitionLeave
-		}
-	}
+  onOperationDefinitionEnter(operationNode: OperationDefinitionNode) {
+    this.calculateCost((operationNode: OperationDefinitionNode));
+  }
 
+  calculateCost(node: OperationDefinitionNode | FieldNode): void {
+    if (node.selectionSet) {
+      node.selectionSet.selections.forEach(childNode => {
+        if (this.argsArray.length === 0) {
+          this.cost += 1;
+          if (childNode.arguments.length == 0) this.argsArray.push(1);
+          else this.argsArray.push(Number(childNode.arguments[0].value.value));
 
-	onOperationDefinitionEnter(operationNode:OperationDefinitionNode){
-		this.calculateCost(operationNode:OperationDefinitionNode)
-	}
+          this.calculateCost(childNode);
+        } else if (childNode.arguments.length > 0) {
+          this.cost += this.argsArray.reduce((product, num) => product * num, 1);
+          this.argsArray.push(Number(childNode.arguments[0].value.value));
+          this.calculateCost(childNode);
+        } else if (childNode.arguments.length == 0 && childNode.selectionSet) {
+          this.cost += this.argsArray.reduce((product, num) => product * num, 1);
+          this.argsArray.push(1);
+          this.calculateCost(childNode, (iteration += 1));
+        }
+      });
+    }
+    // console.log("COST", this.cost);
+  }
 
-	calculateCost(node :OperationDefinitionNode|FieldNode, iteration=0):void{
-		// console.log('iteration ', iteration);
-		if(node.selectionSet){
-			node.selectionSet.selections.forEach(childNode => {
-				if(this.argsArray.length === 0){
-					this.cost += 1;
-					if(childNode.arguments.length == 0){
-						this.argsArray.push(1);
-					}else{
-						this.argsArray.push(Number(childNode.arguments[0].value.value) );
-					}
-					this.calculateCost(childNode, iteration+=1);
-				} else {
-					if(childNode.arguments.length > 0){
-						this.cost += this.argsArray.reduce((product, num) => {
-						return product*=num;
-						},1);
-						this.argsArray.push(Number(childNode.arguments[0].value.value));
-						this.calculateCost(childNode,iteration+=1);
-					}else if(childNode.arguments.length == 0 &&childNode.selectionSet){
-						this.cost += this.argsArray.reduce((product, num) => {
-								return product*=num;
-							},1);
-						this.argsArray.push(1);
-						this.calculateCost(childNode,iteration+=1);
-						}
-				}
+  validateQuery(): void | GraphQLError {
+    let { costLimit, onSuccess, onError } = this.config;
 
-			})
-		}
-		// console.log("COST", this.cost);
-	}
+    if (costLimit < this.cost) {
+      if (typeof onError === 'function') {
+        throw new GraphQLError(onError(this.cost, costLimit));
+      } else {
+        console.log(onError(this.cost, costLimit));
+        throw new GraphQLError(
+          `You are asking for ${this.cost} records. This is ${this.cost -
+            this.costLimit} greater than the permitted request`,
+        );
+      }
+    } else if (typeof onSuccess === 'function') {
+      console.log(onSuccess(this.cost));
+    }
+  }
 
-	validateQuery():void|GraphQLError{
-		let {costLimit, onSuccess, onError} = this.config;
-
-		if(costLimit < this.cost){
-			if(typeof onError === 'function'){
-				// console.log('sjdksd' + onError(this.cost, costLimit));
-				throw new GraphQLError(onError(this.cost, costLimit));
-			} else {
-				console.log(onError(this.cost, costLimit));
-				throw new GraphQLError(`You are asking for ${this.cost} records. This is ${this.cost-this.costLimit} greater than the permitted request`)
-			}
-		} else {
-			if(typeof onSuccess === 'function'){
-				console.log(onSuccess(this.cost));
-			}
-		}
-	}
-
-	onOperationDefinitionLeave(){
-		this.validateQuery();
-	}
+  onOperationDefinitionLeave() {
+    this.validateQuery();
+  }
 }
 
 module.exports = CostLimitComplexity;
--- src/rateLimitRule/indexCost.js
+++ src/rateLimitRule/indexCost.js
@@ -1,12 +1,15 @@
 const CostComplexityOptions = require('./costLimit');
-import type {ValidationContext} from 'graphql';
-import type {costComplexityOptions} from './costLimit';
+import type { ValidationContext } from 'graphql';
+import type { costComplexityOptions } from './costLimit';
 
-const costLimit = (rule: costComplexityOptions):Function =>{
-	return (context:ValidationContext):CostComplexityOptions => {
-		let result = new CostComplexityOptions(context:ValidationContext, rule:costComplexityOptions);
-		return result;
-	}
-}
+const costLimit = (rule: costComplexityOptions): Function => {
+  return (context: ValidationContext): CostComplexityOptions => {
+    let result = new CostComplexityOptions(
+      (context: ValidationContext),
+      (rule: costComplexityOptions),
+    );
+    return result;
+  };
+};
 
 module.exports = costLimit;
