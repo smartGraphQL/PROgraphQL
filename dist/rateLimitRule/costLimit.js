e3a59262fc6e7646009e4d3b3b762172a0dc75cb
roldma <roldma@farmingdale.edu>
2018-08-25 13:04:54 -0400

    prettified?

--- dist/rateLimitRule/costLimit.js
+++ dist/rateLimitRule/costLimit.js
@@ -24,116 +24,75 @@ var CostLimitComplexity = function () {
     };
   }
 
-<<<<<<< HEAD
   _createClass(CostLimitComplexity, [{
     key: 'onOperationDefinitionEnter',
     value: function onOperationDefinitionEnter(operationNode) {
       this.calculateCost(operationNode);
     }
   }, {
-    key: 'calculateCost',
-    value: function calculateCost(node) {
+    key: 'onOperationDefinitionEnter',
+    value: function onOperationDefinitionEnter(operationNode) {
+      this.calculateCost(operationNode);
+    }
+  }, {
+    key: 'updateArgument',
+    value: function updateArgument(node) {
+      console.log(node.kind);
+    }
+  }, {
+    key: 'updateArgumentArray',
+    value: function updateArgumentArray(addConstant, node) {
       var _this = this;
 
-      // console.log('iteration ', iteration);
-      console.log(node.selectionSet);
+      if (addConstant) {
+        this.argsArray.push(1);
+        return;
+      }
+      if (typeof node !== 'undefined' && node.arguments) {
+        node.arguments.forEach(function (argNode) {
+          if (argNode.name === 'first' || 'last') {
+            if (argNode.value.kind === 'IntValue') {
+              var argValue = Number(argNode.value.value);
+              isNaN(argValue) ? '' : _this.argsArray.push(argValue);
+            }
+          }
+        });
+      }
+    }
+  }, {
+    key: 'queryFirstIteration',
+    value: function queryFirstIteration(node) {
+      this.cost += 1;
+      if (node.arguments) this.updateArgumentArray(false, node);else this.updateArgumentArray(true);
+      this.calculateCost(node);
+    }
+  }, {
+    key: 'calculateCost',
+    value: function calculateCost(node) {
+      var _this2 = this;
+
+      if (node.kind === 'FragmentSpread') return;
       if (node.selectionSet) {
-        node.selectionSet.selections.forEach(function (childNode) {
-          if (_this.argsArray.length === 0) {
-            _this.cost += 1;
+        node.selectionSet.selections.forEach(function (child) {
+          if (child.kind === 'Field') {
+            if (_this2.argsArray.length === 0) {
+              _this2.queryFirstIteration(child);
+            } else {
+              if (child.arguments && child.arguments.length > 0) {
+                _this2.cost += _this2.argsArray.reduce(function (product, num) {
+                  return product *= num;
+                }, 1);
+                _this2.updateArgumentArray(false, child);
+                _this2.calculateCost(child);
+              } else if (child.arguments && child.arguments.length == 0 && child.selectionSet) {
+                _this2.cost += _this2.argsArray.reduce(function (product, num) {
+                  return product *= num;
+                }, 1);
+                _this2.updateArgumentArray(true);
 
-            if (childNode.arguments.length == 0) _this.argsArray.push(1);else _this.argsArray.push(Number(childNode.arguments[0].value.value));
-=======
-	_createClass(CostLimitComplexity, [{
-		key: 'onOperationDefinitionEnter',
-		value: function onOperationDefinitionEnter(operationNode) {
-			this.calculateCost(operationNode);
-		}
-	}, {
-		key: 'updateArgument',
-		value: function updateArgument(node) {
-			console.log(node.kind);
-		}
-	}, {
-		key: 'updateArgumentArray',
-		value: function updateArgumentArray(addConstant, node) {
-			var _this = this;
-
-			if (addConstant) {
-				this.argsArray.push(1);
-				return;
-			}
-			if (typeof node !== 'undefined' && node.arguments) {
-				node.arguments.forEach(function (argNode) {
-					if (argNode.name === 'first' || 'last') {
-						if (argNode.value.kind === 'IntValue') {
-							var argValue = Number(argNode.value.value);
-							isNaN(argValue) ? '' : _this.argsArray.push(argValue);
-						}
-					}
-				});
-			}
-		}
-	}, {
-		key: 'queryFirstIteration',
-		value: function queryFirstIteration(node) {
-			this.cost += 1;
-			if (node.arguments) this.updateArgumentArray(false, node);else this.updateArgumentArray(true);
-			this.calculateCost(node);
-		}
-	}, {
-		key: 'calculateCost',
-		value: function calculateCost(node) {
-			var _this2 = this;
-
-			if (node.kind === 'FragmentSpread') return;
-			if (node.selectionSet) {
-				node.selectionSet.selections.forEach(function (child) {
-					if (child.kind === 'Field') {
-						if (_this2.argsArray.length === 0) {
-							_this2.queryFirstIteration(child);
-						} else {
-							if (child.arguments && child.arguments.length > 0) {
-								_this2.cost += _this2.argsArray.reduce(function (product, num) {
-									return product *= num;
-								}, 1);
-								_this2.updateArgumentArray(false, child);
-								_this2.calculateCost(child);
-							} else if (child.arguments && child.arguments.length == 0 && child.selectionSet) {
-								_this2.cost += _this2.argsArray.reduce(function (product, num) {
-									return product *= num;
-								}, 1);
-								_this2.updateArgumentArray(true);
-
-								_this2.calculateCost(child);
-							}
-						}
-					}
-				});
-			}
-		}
-	}, {
-		key: 'validateQuery',
-		value: function validateQuery() {
-			var _config = this.config,
-			    costLimit = _config.costLimit,
-			    onSuccess = _config.onSuccess,
-			    onError = _config.onError;
->>>>>>> 290851f8e628c8c28a8d2bf524867e7c89cf80da
-
-            _this.calculateCost(childNode);
-          } else if (childNode.arguments && childNode.arguments.length > 0) {
-            _this.cost += _this.argsArray.reduce(function (product, num) {
-              return product * num;
-            }, 1);
-            _this.argsArray.push(Number(childNode.arguments[0].value.value));
-            _this.calculateCost(childNode);
-          } else if (childNode.arguments && childNode.arguments.length == 0 && childNode.selectionSet) {
-            _this.cost += _this.argsArray.reduce(function (product, num) {
-              return product * num;
-            }, 1);
-            _this.argsArray.push(1);
-            _this.calculateCost(childNode);
+                _this2.calculateCost(child);
+              }
+            }
           }
         });
       }
@@ -143,16 +102,18 @@ var CostLimitComplexity = function () {
     value: function validateQuery() {
       // const { costLimit, onSuccess, onError } = this.config;
 
-<<<<<<< HEAD
-      if (this.config.costLimit < this.cost) {
-        console.log('LIMIT', this.config.costLimit, '\nACTUAL COST', this.cost);
-        if (typeof onError === 'function') throw new GraphQLError('ERRORSROS IS FUNCTION CONDITOINAL');else {
-          console.log('ERRROROSRO');
-          // console.log(this.config.onError(this.cost, this.config.costLimit));
-          throw new GraphQLError('Actual cost is greater than set cost limit.');
+      if (costLimit < this.cost) {
+        if (typeof onError === 'function') {
+          // console.log('sjdksd' + onError(this.cost, costLimit));
+          throw new GraphQLError(onError(this.cost, costLimit));
+        } else {
+          // console.log(onError(this.cost, costLimit));
+          throw new GraphQLError('You are asking for ' + this.cost + ' records. This is ' + (this.cost - costLimit) + ' greater than the permitted request');
         }
-      } else if (typeof this.config.onSuccess === 'function') {
-        console.log(this.config.onSuccess(this.cost));
+      } else {
+        if (typeof onSuccess === 'function') {
+          console.log(onSuccess(this.cost));
+        }
       }
     }
   }, {
@@ -162,28 +123,6 @@ var CostLimitComplexity = function () {
       return this.validateQuery();
     }
   }]);
-=======
-			if (costLimit < this.cost) {
-				if (typeof onError === 'function') {
-					// console.log('sjdksd' + onError(this.cost, costLimit));
-					throw new GraphQLError(onError(this.cost, costLimit));
-				} else {
-					// console.log(onError(this.cost, costLimit));
-					throw new GraphQLError('You are asking for ' + this.cost + ' records. This is ' + (this.cost - costLimit) + ' greater than the permitted request');
-				}
-			} else {
-				if (typeof onSuccess === 'function') {
-					console.log(onSuccess(this.cost));
-				}
-			}
-		}
-	}, {
-		key: 'onOperationDefinitionLeave',
-		value: function onOperationDefinitionLeave() {
-			this.validateQuery();
-		}
-	}]);
->>>>>>> 290851f8e628c8c28a8d2bf524867e7c89cf80da
 
   return CostLimitComplexity;
 }();
--- dist/rateLimitRule/indexCost.js
+++ dist/rateLimitRule/indexCost.js
@@ -6,9 +6,9 @@ var _require = require('graphql'),
     GraphQLError = _require.GraphQLError;
 
 var costLimit = function costLimit(rule) {
-  if (rule.costLimit <= 0) throw new GraphQLError('Cost limit must be greater than 0');
-  // return 'Cost limit must be greater than 0';
-
+  if (rule.costLimit <= 0) {
+    throw new GraphQLError('Cost limit must be greater than 0');
+  }
   return function (context) {
     var result = new CostComplexityOptions(context, rule);
     return result;
