d97ca85effde10fa5f6cf23fb6f6a4597919f30c
Julia Ieshtokina <julia.ieshtokina@gmail.com>
2018-08-28 16:21:44 -0400

    depth tests added

--- test/test.js
+++ test/test.js
@@ -8,13 +8,12 @@ const {
 } = require('graphql');
 
 const CostLimitComplexity = require('../src/rateLimitRule/costLimit');
+const DepthLimitComplexity = require('../src/depthRule/depthComplexity');
 const schema = require('./mock_schema/schema');
 const costLimit = require('../src/rateLimitRule/indexCost');
+const depthLimit = require('../src/depthRule/indexDepth'); 
 
-/**
- * Complexity Cost Tests
- */
-describe('Query Complexity Analysis', () => {
+describe('Query Cost Analysis', () => {
   const typeInfo = new TypeInfo(schema);
 
   test('initial cost should be 0', () => {
@@ -30,9 +29,6 @@ describe('Query Complexity Analysis', () => {
   });
 
   test('should calculate cost of a simple query without arguments', () => {
-    const ruleCost = {
-      costLimit: 10,
-    };
     const ast = parse(`
       query {
         item {
@@ -44,7 +40,7 @@ describe('Query Complexity Analysis', () => {
     `);
 
     const context = new ValidationContext(schema, ast, typeInfo);
-    const complexity = new CostLimitComplexity(context, ruleCost);
+    const complexity = new CostLimitComplexity(context, { costLimit: 10 });
     visit(ast, visitWithTypeInfo(typeInfo, complexity));
     expect(complexity.cost).toBe(1);
   });
@@ -103,8 +99,6 @@ describe('Query Complexity Analysis', () => {
     visit(ast, visitWithTypeInfo(typeInfo, complexity));
     expect(complexity.cost).toBe(6);
   });
-
-  
 
   test('should calculate cost of a complex query if arguments are provided', () => {
     const ast = parse(`
@@ -201,5 +195,191 @@ describe('Query Complexity Analysis', () => {
     visit(ast, visitWithTypeInfo(typeInfo, complexity));
     expect(complexity.cost).toBe(1);
   });
+
+});
+
+
+describe('Query Depth Analysis', () => {
+  const typeInfo = new TypeInfo(schema);
+
+  test('initial depth should be 0', () => {
+    const context = new ValidationContext(schema);
+    const complexity = new DepthLimitComplexity(context);
+    expect(complexity.actualDepth).toBe(0);
+  });
+
+  test('depth limit set by user should be greater than 0', () => {
+    expect(() => {
+      depthLimit({ depthLimit: 0 });
+    }).toThrowError(GraphQLError);
+  });
+
+  test('should calculate depth of a simple query without arguments', () => {
+    const ast = parse(`
+      query {
+        item {
+          scalar {
+            name
+          }
+        }
+      }
+    `);
+    const context = new ValidationContext(schema, ast, typeInfo);
+    const complexity = new DepthLimitComplexity(context, { depthLimit: 5 });
+    visit(ast, visitWithTypeInfo(typeInfo, complexity));
+    expect(complexity.actualDepth).toBe(3);
+  });
+
+  test('should calculate depth of a complex query without arguments', () => {
+    const ast = parse(`
+    query {
+      viewer {
+        login
+        repositories {
+          edges {
+            node {
+              id
+    
+              issues {
+                edges {
+                  node {
+                    id
+    
+                    labels {
+                      edges {
+                        node {
+                          id
+                          name
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    `);
+    const context = new ValidationContext(schema, ast, typeInfo);
+    const complexity = new DepthLimitComplexity(context, { depthLimit: 15 });
+    visit(ast, visitWithTypeInfo(typeInfo, complexity));
+    expect(complexity.actualDepth).toBe(11);
+  });
+
+  test('should calculate depth of a simple query if arguments are provided', () => {
+    const ast = parse(`
+      query {
+        item(first:5) {
+          variableScalar (last:25) {
+           item 
+          }
+        }
+      }
+    `);
+    const context = new ValidationContext(schema, ast, typeInfo);
+    const complexity = new DepthLimitComplexity(context, { depthLimit: 4 });
+    visit(ast, visitWithTypeInfo(typeInfo, complexity));
+    expect(complexity.actualDepth).toBe(3);
+  });
+
+  test('should calculate depth of a complex query if arguments are provided', () => {
+    const ast = parse(`
+    query {
+      viewer {
+        login
+        repositories(first: 100) {
+          edges {
+            node {
+              id
+              issues(first: 50) {
+                edges {
+                  node {
+                    id
+                    labels(first: 60) {
+                      edges {
+                        node {
+                          id
+                          name
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    `);
+    const context = new ValidationContext(schema, ast, typeInfo);
+    const complexity = new DepthLimitComplexity(context, { depthLimit: 12 });
+    visit(ast, visitWithTypeInfo(typeInfo, complexity));
+    expect(complexity.actualDepth).toBe(11);
+  });
+
+   test('should throw an error if actual query depth is greater than depth limit set by user', () => {
+    const ast = parse(`
+      query{
+        item {
+          scalar(first: 25) {
+            name (first: 10)
+          }
+        }
+      }
+    `);
+    const context = new ValidationContext(schema, ast, typeInfo);
+    const complexity = new DepthLimitComplexity(context, { depthLimit: 1 });
+    expect(() => {
+      visit(ast, visitWithTypeInfo(typeInfo, complexity));
+    }).toThrowError(GraphQLError);
+  });
+
+  test('should properly calculate depth on queries containing fragments', () => {
+    const ast = parse(`
+      query {
+        scalar {
+          ...QueryFragment
+        }
+      }
+
+      fragment QueryFragment on Query {
+        name {
+          lastName
+          firstName(first:10) {
+            id
+          }
+        }
+        address
+        phoneNumber
+      }
+    `);
+    const context = new ValidationContext(schema, ast, typeInfo);
+    const complexity = new DepthLimitComplexity(context, { depthLimit: 5 });
+    visit(ast, visitWithTypeInfo(typeInfo, complexity));
+    expect(complexity.actualDepth).toBe(4);
+  });
+
+  test('should properly calculate depth on queries containing interfaces', () => {
+   const ast = parse(`
+     query {
+       interface {
+        name
+        ... on NameInterface {
+          name
+        }
+        item {
+          scalar
+        }
+      }
+    }
+  `);
+  const context = new ValidationContext(schema, ast, typeInfo);
+  const complexity = new DepthLimitComplexity(context, { depthLimit: 5 });
+  visit(ast, visitWithTypeInfo(typeInfo, complexity));
+  expect(complexity.actualDepth).toBe(3);
+});
 
 });
